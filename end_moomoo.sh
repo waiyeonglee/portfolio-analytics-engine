0898a65ee2e92a2fc1b5b063777819d41dc64f20
waiyeong <waiyeonglee@gmail.com>
2026-07-28 23:42:21 +0800

    bash scripts rework

--- end_moomoo.sh
+++ end_moomoo.sh
@@ -7,7 +7,8 @@ DIR="/home/ubuntu/moomoo_scripts/logs"
 today=$(date +%Y-%m-%d)
 
 # Save all logs of the service to a file
-journalctl -u moomoo -b 0 --no-pager > "$DIR/$today - moomoo_journal.txt"
+journalctl -u moomoo_simulate -b 0 --no-pager > "$DIR/$today - moomoo_simulate_journal.txt"
+journalctl -u moomoo_real -b 0 --no-pager > "$DIR/$today - moomoo_real_journal.txt"
 
 # Find files with today's date
 find "$DIR" -type f -name "*$today*" | while IFS= read -r file; do
---
+++ start_moomoo_simulate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copy latest main.py to EC2
+aws s3 cp "s3://moomoos3/main.py" "/home/ubuntu/moomoo_scripts/main.py"
+
+# Find and delete log files older than 30 days
+# find /home/ubuntu/moomoo_scripts/logs/ -type f -mtime +30 -delete
+# Run main.py
+/home/ubuntu/moomoo/bin/python3 -u /home/ubuntu/moomoo_scripts/main.py --env "real" --live
