9c9d2cfd0160bb3d28f3bb2ab9b7c836e6836d80
waiyeong <waiyeonglee@gmail.com>
2026-09-25 10:47:43 +0800

    folder reorg

--- jobs/start_moomoo_simulate.sh
+++ jobs/start_moomoo_simulate.sh
@@ -8,4 +8,4 @@ aws s3 sync "s3://moomoos3/scripts/" "/home/ubuntu/moomoo_scripts/scripts/" \
 # Find and delete log files older than 30 days
 # find /home/ubuntu/moomoo_scripts/logs/ -type f -mtime +30 -delete
 # Run main.py
-/home/ubuntu/moomoo/bin/python3 -u /home/ubuntu/moomoo_scripts/main.py --live
+/home/ubuntu/moomoo/bin/python3 -u /home/ubuntu/moomoo_scripts/scripts/main.py --live
